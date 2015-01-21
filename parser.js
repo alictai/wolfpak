0a480788a2ef21bd7183467679406533c2336f00
Broratheon <hyungseo0107@gmail.com>
2015-01-20 23:09:31 -0500

    parsed the courseLocations.csv file

--- parser.js
+++ parser.js
@@ -1,7 +1,23 @@
 var parse_courses = function()
 {
-	d3.csv("CourseLocations.csv", function(courses)
-		{
-			console.log("courses:", courses)
-		})
+	if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", "CourseLocations.csv", false );
+    xmlhttp.send();  
+
+	return d3.csv.parse(xmlhttp.responseText);
+
 }
