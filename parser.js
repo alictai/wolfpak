e5c1d4c97581b9ec207510ca4468bee7beb2c648
Broratheon <hyungseo0107@gmail.com>
2015-01-21 21:34:40 -0500

    parsing buildings

--- parser.js
+++ parser.js
@@ -1,4 +1,4 @@
-var parse_courses = function()
+function parse_courses()
 {
 
     // This will need to happen when we move on to use
@@ -22,7 +22,47 @@ var parse_courses = function()
     xmlhttp.open("GET", "CourseLocations.csv", false );
     xmlhttp.send();  
     */
-    
-	return d3.csv.parse(courseLocations);
 
+	var parsed_data = d3.csv.parse(courseLocations);
+
+    parsed_data.forEach(function(element, index, array)
+        {
+            // Extract just the building code without room number.
+            element.Location = element.Location.split(" ")[0];
+        });
+
+    return parsed_data;
+}
+
+// Will also need to take an input of selected departments eventually.
+// For now, just assume all of them are selected.
+function split_buildings(courses)
+{
+    var buildings = [];
+    courses.forEach(function(element, index, array)
+        {
+            var index = contains_building(buildings, element.Location);
+            // -1 means that buildings isn't in the array yet.
+            if (index == -1){
+                buildings.push({"Building": element.Location, "Enrolled" : element.Enrolled});
+            } else {
+                buildings[index].Enrolled += element.Enrolled;
+            }
+        });
+
+    console.log(buildings);
+}
+
+function contains_building(buildings, location)
+{
+    var building_index = -1;
+
+    buildings.forEach(function(element, index, array)
+        {
+            if(element.Location == location){
+                building_index = index;
+            }
+        });
+
+    return building_index;
 }
